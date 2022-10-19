4ef6f221b643a93f9c5279e31b0a2d5e4cef1ab6
PedroH-Teixeira <teixeira.phenrique@gmail.com>
2022-10-19 16:00:18 -0300

    Atualização do caminho na nuvem

--- dados.js
+++ dados.js
@@ -1,4 +1,3 @@
-
     function buscar(){
         
         c = document.getElementById('chapa').value;
@@ -37,35 +36,25 @@
                      document.getElementById(corrigeCount).style.display = 'none';
                      console.log('Essa imagem foi ocultada porque não existe no servidor');
                      };
-                }
+                } 
 
                 document.write("<figure class='gallery-element'id="+count+"><a href='\\\\10.75.0.242\\photo\\Presentation\\"+count+".png'><img id="+count+" src='\\\\10.75.0.242\\photo\\Presentation\\"+count+".png'><figcaption><h1>"+count+"</h1></figcaption></a></figure><!--gallery-element-->");
             }     
        
             if(opcao==="slabsmith"){
-                var chapa = JSON.parse(dados);
+                
+                is_img("https://stgimageneslotespro.blob.core.windows.net/cosentino/slab/"+count+".png");
 
-                for(count2= 0; count2<=775890 ; count2++){
-                    if (count === chapa[count2].lote){
-
-                        is_img("\\\\10.75.0.243\\PhotoStation\\ssData\\"+chapa[count2].pasta+"\\Image.jpg");
-
-                        function is_img(file) {
-                             var corrigeCount = count;
-                             var img = document.createElement('img');
-                             img.src = file;
-                             img.onerror = function() {
-                             document.getElementById(corrigeCount).style.display = 'none';
-                             console.log('Essa imagem foi ocultada porque não existe no servidor');
-                             };
-                        }
-
-
-                        document.write("<figure class='gallery-element' id="+count+"><a href='\\\\10.75.0.243\\PhotoStation\\ssData\\"+chapa[count2].pasta+"\\Image.jpg'><img src='\\\\10.75.0.243\\\PhotoStation\\ssData\\"+chapa[count2].pasta+"\\Image.jpg'><figcaption><h1>"+count+"</h1></figcaption></a></figure><!--gallery-element-->");
-
-                    }
-                }
-              //  document.write("<figure class='gallery-element' id="+count+"><a href='\\\\10.75.0.242\\photo\\Photos Slabsmith\\Presentation\\"+count+".jpg'><img src='\\\\10.75.0.242\\photo\\Photos Slabsmith\\Presentation\\"+count+".jpg'><figcaption><h1>"+count+"</h1></figcaption></a></figure><!--gallery-element-->");
+                function is_img(file) {
+                     var corrigeCount = count;
+                     var img = document.createElement('img');
+                     img.src = file;
+                     img.onerror = function() {
+                     document.getElementById(corrigeCount).style.display = 'none';
+                     console.log('Essa imagem foi ocultada porque não existe no servidor');
+                     };
+                } 
+                document.write("<figure class='gallery-element' id="+count+"><a href='https://stgimageneslotespro.blob.core.windows.net/cosentino/slab/"+count+".png'><img src='https://stgimageneslotespro.blob.core.windows.net/cosentino/slab/"+count+".png'><figcaption><h1>"+count+"</h1></figcaption></a></figure><!--gallery-element-->");
             }
         }
         }else{
